6b45f54ecb38cac1c262263ca4eec1221636d420
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-09-08 23:48:22 +0200

    add delete exam by id endpoint

--- app/api/exams/by-exam-id/[examId]/route.ts
+++ app/api/exams/by-exam-id/[examId]/route.ts
@@ -3,10 +3,13 @@ import { ApiResponseError, ApiResponseSuccess } from "@/types/api"
 import { Exam } from "@/types/models"
 import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { examId: string } },
-): Promise<NextResponse> {
+type ParamsType = {
+  params: {
+    examId: string
+  }
+}
+
+export async function GET(request: NextRequest, { params }: ParamsType): Promise<NextResponse> {
   try {
     const { examId } = await params
 
@@ -42,5 +45,46 @@ export async function GET(
       },
     }
     return NextResponse.json(errorResponse, { status: 500 })
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: ParamsType) {
+  const { examId } = await params
+
+  if (!examId) {
+    const errorResponse: ApiResponseError = {
+      success: false,
+      error: {
+        code: "EXAM_ID_REQUIRED",
+        message: "Se requiere el ID del examen",
+      },
+    }
+    return NextResponse.json(errorResponse, { status: 400 })
+  }
+
+  try {
+    await prisma.exam.delete({
+      where: {
+        id_examen: examId,
+      },
+    })
+
+    const successResponse: ApiResponseSuccess<null> = {
+      success: true,
+      message: "Examen eliminado correctamente.",
+      data: null,
+    }
+
+    return NextResponse.json(successResponse, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    const errorResponse: ApiResponseError = {
+      success: false,
+      error: {
+        code: "EXAM_NOT_FOUND",
+        message: "No se pudo eliminar el examen. Puede que no exista.",
+      },
+    }
+    return NextResponse.json(errorResponse, { status: 404 })
   }
 }
