f22737e9786b3323695be245ef7f58ed4ed2e6a6
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-10-05 00:00:34 +0200

    feature: add link to header icon

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,6 +5,7 @@ import { Menu, X } from "lucide-react"
 import MenuButton from "@/components/MenuButton"
 import { cn } from "@/lib/utils"
 import { useState } from "react"
+import Link from "next/link"
 
 interface Props {
   className?: string
@@ -16,7 +17,9 @@ export default function Header({ className }: Props) {
 
   return (
     <header className={cn("flex w-full flex-row items-center justify-between", className)}>
-      <Image src="/images/icon.png" alt="Icono de un alumno estudiando." width={34} height={34} />
+      <Link className="cursor-pointer" href={"/"}>
+        <Image src="/images/icon.png" alt="Icono de un alumno estudiando." width={34} height={34} />
+      </Link>
       <MenuButton onClick={() => setOpen((prev) => !prev)}>
         {isOpen ? <X /> : <Menu />}
         <span>{isOpen ? "Close" : "Open"}</span>
