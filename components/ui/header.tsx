be34be7e5f5d92b80a30e9af3ddbec84b8a74fd9
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-10-11 20:57:16 +0200

    add login button

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,29 +1,62 @@
 "use client"
 
 import Image from "next/image"
-import { Menu, X } from "lucide-react"
-import MenuButton from "@/components/MenuButton"
+import { Shapes } from "lucide-react"
 import { cn } from "@/lib/utils"
-import { useState } from "react"
 import Link from "next/link"
+import Button from "@/components/ui/button"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 interface Props {
   className?: string
 }
 
 export default function Header({ className }: Props) {
-  const [open, setOpen] = useState(false)
-  const isOpen = open
+  const { data: session } = useSession()
+
+  const handleSessionButtonAction = () => {
+    if (!session)
+      return signIn(
+        "discord",
+        {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+        { callbackUrl: "/" },
+      )
+
+    return signOut()
+  }
+
+  const buttonText = session ? "Cerrar sesión" : "Iniciar sesión"
 
   return (
     <header className={cn("flex w-full flex-row items-center justify-between", className)}>
-      <Link className="cursor-pointer" href={"/"}>
-        <Image src="/images/icon.png" alt="Icono de un alumno estudiando." width={34} height={34} />
-      </Link>
-      <MenuButton onClick={() => setOpen((prev) => !prev)}>
-        {isOpen ? <X /> : <Menu />}
-        <span>{isOpen ? "Close" : "Open"}</span>
-      </MenuButton>
+      <div className="flex flex-row items-center gap-1">
+        <Link className="flex cursor-pointer flex-row items-end gap-2" href={"/"}>
+          <Image
+            src="/images/icon.png"
+            alt="Icono de un alumno estudiando."
+            width={34}
+            height={34}
+          />
+          <span className="text-xl">examio</span>
+        </Link>
+
+        <span className="decoration-yellow self-end px-2 py-1 text-sm italic underline decoration-2">
+          beta
+        </span>
+      </div>
+      <div className="button-after relative">
+        <Button
+          className="hover:bg-purple bg-yellow flex translate-x-1.5 -translate-y-1.5 transform cursor-pointer flex-row items-center gap-2 rounded-[4px] p-2 font-semibold text-white hover:translate-0 active:scale-90"
+          onClick={handleSessionButtonAction}
+        >
+          {buttonText}
+          <Shapes />
+        </Button>
+      </div>
     </header>
   )
 }
