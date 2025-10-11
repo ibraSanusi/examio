862b4c375e97a1106a84d29c97a180cf2e988fef
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-10-11 20:55:22 +0200

    change some styles in login page

--- app/pages/auth/login/page.tsx
+++ app/pages/auth/login/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Button from "@/components/ui/button"
+import { Discord } from "@/components/ui/icons/Discord"
 import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function LoginPage() {
@@ -20,13 +22,22 @@ export default function LoginPage() {
     return signOut()
   }
 
-  const buttonText = session ? "Sign out" : "Sing in"
+  const buttonText = session ? "Sign out!" : "Empieza a jugar ahora!"
 
   return (
-    <div>
+    <section>
       <h1>Login con Discord</h1>
+      <p>¡Aprender es divertido con Examio!</p>
       {session && <p>Ya estás logueado como {session.user?.name}</p>}
-      <button onClick={handleSessionButtonAction}>{buttonText}</button>
-    </div>
+      <div className="flex flex-row items-center gap-4">
+        <Button
+          className="hover:bg-yellow bg-light-brown flex flex-row items-center gap-3"
+          onClick={handleSessionButtonAction}
+        >
+          {buttonText}
+          <Discord className="size-5" />
+        </Button>
+      </div>
+    </section>
   )
 }
