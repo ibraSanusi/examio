6ca39fc152be166f702fb5d9589a2bfb1879d437
Ibrahim Sanusi <ibrahim@fullcircle.es>
2025-10-12 18:51:41 +0200

    style: change title and paragraph

--- app/pages/auth/login/page.tsx
+++ app/pages/auth/login/page.tsx
@@ -25,13 +25,16 @@ export default function LoginPage() {
   const buttonText = session ? "Sign out!" : "Empieza a jugar ahora!"
 
   return (
-    <section>
-      <h1>Login con Discord</h1>
-      <p>¡Aprender es divertido con Examio!</p>
-      {session && <p>Ya estás logueado como {session.user?.name}</p>}
-      <div className="flex flex-row items-center gap-4">
+    <section className="mx-auto mt-26 flex w-full max-w-4xl flex-col gap-6 px-4 py-3">
+      <div className="space-y-2">
+        <h1 className="text-center text-5xl font-bold">EXAM.IO clase online</h1>
+        <p className="text-center text-xl font-semibold italic">
+          Plataforma desarrollada para que los más jóvenes pongan a prueba sus conocimientos.
+        </p>
+      </div>
+      <div className="flex w-full flex-row items-center justify-center gap-4">
         <Button
-          className="hover:bg-yellow bg-light-brown flex flex-row items-center gap-3"
+          className="hover:bg-yellow flex flex-row items-center gap-3 bg-black duration-75 ease-in hover:scale-110 active:scale-90"
           onClick={handleSessionButtonAction}
         >
           {buttonText}
